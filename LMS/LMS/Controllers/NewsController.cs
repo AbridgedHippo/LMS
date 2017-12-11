7679cd957d2828dc7d562d2d86dc1826df3f0425
oliver eriksson <virus.oliver@gmail.com>
2017-12-11 15:16:21 +0100

    Newsfeed Init

---
+++ LMS/LMS/Controllers/NewsController.cs
@@ -0,0 +1,35 @@
+﻿using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace LMS.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Index(string RSSURL)
+        //{
+        //    WebClient wclient = new WebClient();
+        //    string RSSData = wclient.DownloadString(RSSURL);
+
+        //    XDocument xml = XDocument.Parse(RSSData);
+        //    var RSSFeedData = (from x in xml.Descendants("item")
+        //                       select new RSSFeed
+        //                       {
+        //                           Title = ((string)x.Element("title")),
+                                   
+        //                       });
+        //}
+    }
+}
---
+++ LMS/LMS/Models/Newsfeed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models
+{
+    public class Newsfeed
+    {
+        public string Title { get; set; }
+        public DateTime PubDate { get; set; }
+        public string BreadText { get; set; }
+    }
+}
